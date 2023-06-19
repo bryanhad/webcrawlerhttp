3527e7171414d78d686868219741f002e85d35cb
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-06-20 00:23:30 +0700

    getUrlsFromHtml function and test done

--- crawl.js
+++ crawl.js
@@ -1,3 +1,38 @@
+const {JSDOM} = require('jsdom')
+
+function getUrlsFromHtml(htmlBody, baseUrl){
+    const urls = []
+    const {window} = new JSDOM(htmlBody)
+    const linkElements = window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0,1) === '/') {
+            const absoluteUrl = `${baseUrl}${linkElement.href}`
+
+            try {
+                const urlObj = new URL(absoluteUrl)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`Error with ${linkElement.href} relative url: ${err.message}`)
+            }
+        } else {
+
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`Error with "${linkElement.href}" absolute url: ${err.message}`)
+            }
+        }
+
+        // if (linkElement.href.includes('http')) {
+        //     urls.push(linkElement.href)
+        // } else {
+        //     urls.push(`${baseUrl}${linkElement.href}`)
+        // }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostpath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +43,5 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL, getUrlsFromHtml
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getUrlsFromHtml} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip https', () => {
@@ -26,5 +26,85 @@ test('normalizeURL strip http', () => {
     const input = 'http://cleannfresh.com/blogs/'
     const actual = normalizeURL(input)
     const expected = 'cleannfresh.com/blogs'
+    expect(actual).toEqual(expected)
+})
+
+test('getUrlsFromHtml, absolute', () => {
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <h1>Wellcome to cleannfresh website!</h1>
+            <a href='https://cleannfresh.com/blogs'>
+                see our blogs!
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = 'https://cleannfresh.com'
+    const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl)
+    const expected = ['https://cleannfresh.com/blogs']
+    expect(actual).toEqual(expected)
+})
+
+test('getUrlsFromHtml, relative', () => {
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <h1>Wellcome to cleannfresh website!</h1>
+            <a href='/blogs'>
+                see our blogs!
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = 'https://cleannfresh.com'
+    const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl)
+    const expected = ['https://cleannfresh.com/blogs']
+    expect(actual).toEqual(expected)
+})
+
+test('getUrlsFromHtml, multiple links', () => {
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <h1>Wellcome to cleannfresh website!</h1>
+            <a href='/blogs'>
+                see our blogs!
+            </a>
+            <a href='/items'>
+                see our blogs!
+            </a>
+            <a href='https://youtube.com/cleannfresh/videos'>
+                see our youtube channel!
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = 'https://cleannfresh.com'
+    const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl)
+    const expected = ['https://cleannfresh.com/blogs', 'https://cleannfresh.com/items', 'https://youtube.com/cleannfresh/videos']
+    expect(actual).toEqual(expected)
+})
+
+test('getUrlsFromHtml, invalid link', () => {
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <h1>Wellcome to cleannfresh website!</h1>
+            <a href='/blogs'>
+                see our blogs!
+            </a>
+            <a href='ObviouslyNotALink'>
+                see our blogs!
+            </a>
+            <a href='https://youtube.com/cleannfresh/videos'>
+                see our youtube channel!
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = 'https://cleannfresh.com'
+    const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl)
+    const expected = ['https://cleannfresh.com/blogs', 'https://youtube.com/cleannfresh/videos']
     expect(actual).toEqual(expected)
 })
